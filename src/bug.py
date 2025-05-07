21d103a118d0ffc69be792d27cf17379336e6656
. <Priyadarshan@wolterskluwer.com>
2025-05-07 17:07:50 +0530

    Final test

--- src/bug.py
+++ src/bug.py
@@ -9,5 +9,5 @@ def buggy_function():
 
 if __name__ == "__main__":
     buggy_function()
-    print("This is the main function")
+    print("This is the main function"
     subprocess.run("echo 'Hello from the main function!'", shell=True)
